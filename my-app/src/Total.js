2d7d5b42eae5ae6fa8f9e8a1259b94127237b89a
DanielToledo-99 <toledo992905@gmail.com>
2021-03-26 20:02:47 -0500

    Punto 3: Creacion de Header, Content y Total

---
+++ my-app/src/Total.js
@@ -0,0 +1,10 @@
+const Total = (props) =>{
+    return (
+        <div>
+            <p>
+            <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+            </p>
+        </div>
+    )
+}
+export default Total
