30c6509d1f9a7bc520fe920f1c9bc5c1748a1be8
DanielToledo-99 <toledo992905@gmail.com>
2021-03-26 20:06:25 -0500

    Punto 4: Creacion de Parts.js

--- my-app/src/Content.js
+++ my-app/src/Content.js
@@ -1,9 +1,11 @@
+import Parts from './Parts.js'
+import App from './index.js'
 const Content = (props) =>{
     return (
         <div>
-            <p>{props.part1} {props.exercises1}</p>
-            <p>{props.part2} {props.exercises2}</p>
-            <p>{props.part3} {props.exercises3}</p>
+            <Parts part1={props.part1 } exercises1={props.exercises1}/>
+            <Parts part2={props.part2 } exercises2={props.exercises2}/>
+            <Parts part3={props.part3 } exercises3={props.exercises3}/>
         </div>
     )
 }
